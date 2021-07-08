c8d960453bd7b9d8c66aa9183d728f7bb92300eb
artem <ar.semenov@qsoft.ru>
2021-07-08 19:00:31 +0300

    1-4

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: '5603',
+    image: 'img/toothbrush.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: '4343',
+    image: 'img/radio.jpg',
+  },
+];
