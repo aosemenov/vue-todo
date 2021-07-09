f53daf6361ec40e441cddda52b88086a356bda57
artem <ar.semenov@qsoft.ru>
2021-07-09 18:39:10 +0300

    '4-5'

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,41 @@
 <template>
-  <section class="catalog">
-
-
-    <ProductList :products="products" />
-
-
-    <ul class="catalog__pagination pagination">
-      <li class="pagination__item">
-        <a class="pagination__link
-        pagination__link--arrow pagination__link--disabled"
-           aria-label="Предыдущая страница">
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-left"></use>
-          </svg>
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--current">
-          1
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          2
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          3
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          4
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          ...
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          10
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--arrow"
-           href="#" aria-label="Следующая страница">
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </li>
-    </ul>
-  </section>
+  <component :is="currentPageComponent" :page-params="currentPageParams"></component>
 </template>
 
 <script>
+import MainPage from './pages/MainPage.vue';
+import ProductPage from './pages/ProductPage.vue';
+import NotFoundPage from './pages/NotFoundPage.vue';
+import eventBus from './eventBus';
 
-import products from './data/products';
-import ProductList from './components/ProductList';
-
+const routes = {
+  main: 'MainPage',
+  product: 'ProductPage',
+};
 
 export default {
-  name: 'App',
-  components: { ProductList },
   data() {
     return {
-      products,
+      currentPage: 'main',
+      currentPageParams: {},
     };
+  },
+  components: {
+    MainPage, ProductPage, NotFoundPage,
+  },
+  methods: {
+    gotoPage(pageName, pageParams) {
+      this.currentPage = pageName;
+      this.currentPageParams = pageParams || {};
+    },
+  },
+  computed: {
+    currentPageComponent() {
+      return routes[this.currentPage] || 'NotFoundPage';
+    },
+  },
+  created() {
+    eventBus.$on('gotoPage', (pageName, pageParams) => this.gotoPage(pageName, pageParams));
   },
 };
 </script>
---
+++ src/components/ProductItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <li class="catalog__item">
+    <a class="catalog__pic" href="#"
+       @click.prevent="gotoPage('product', {id: product.id})">
+      <img :src="product.image"   :alt="product.title">
+    </a>
+
+    <h3 class="catalog__title">
+      <a href="#">
+        {{ product.title }}
+      </a>
+    </h3>
+
+    <span class="catalog__price">
+              {{ product.price | numberFormat}}
+            </span>
+
+    <ul class="colors colors--black">
+      <li class="colors__item">
+        <label class="colors__label">
+          <input class="colors__radio sr-only"
+                 type="radio" value="#73B6EA" v-model="color">
+          <span class="colors__value" style="background-color: #73B6EA;">
+                  </span>
+        </label>
+      </li>
+      <li class="colors__item">
+        <label class="colors__label">
+          <input class="colors__radio sr-only"
+                 type="radio" value="#8BE000" v-model="color">
+          <span class="colors__value" style="background-color: #8BE000;">
+                  </span>
+        </label>
+      </li>
+      <li class="colors__item">
+        <label class="colors__label" type="radio" value="#222" >
+          <input class="colors__radio sr-only"
+                 type="radio" value="#222" v-model="color">
+          <span class="colors__value" style="background-color: #222;">
+                  </span>
+        </label>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+import gotoPage from '../helpers/gotoPage';
+import numberFormat from '../helpers/numberFormat';
+
+export default {
+  name: 'ProductItem',
+  data() {
+    return {
+      color: '#73B6EA',
+    };
+  },
+  filters: {
+    numberFormat,
+  },
+  props: ['product'],
+  methods: {
+    gotoPage,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
